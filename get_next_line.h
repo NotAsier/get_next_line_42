fbda5d9b20cb9a7405359940bf4c2e0e6f3b0b12
NotAsier <asierarranz99@gmail.com>
2023-08-28 21:45:17 +0200

    WIP

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aarranz- <aarranz-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: arranz <arranz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 10:26:45 by aarranz-          #+#    #+#             */
-/*   Updated: 2023/08/28 12:32:54 by aarranz-         ###   ########.fr       */
+/*   Updated: 2023/08/28 21:29:00 by arranz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,19 @@
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 6
 # endif
 
+size_t	ft_detect(const char *s);
+size_t	ft_strlen_line(const char *str);
 char	*get_next_line(int fd);
+
+int main(void);
+
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
 
 #endif // GET_NEXT_LINE_H
